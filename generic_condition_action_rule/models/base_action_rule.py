b7ee469de95251e46b77a26eb64e2c326ad8d371
Дмитро Катюха <dmytro.katyukha@maao.com.ua>
2017-07-04 10:29:41 +0300

    generic_condition_action_rule: Handle pylint warnings
    Do not show api-v7-method warning on overridden onchanges

--- generic_condition_action_rule/models/base_action_rule.py
+++ generic_condition_action_rule/models/base_action_rule.py
@@ -12,7 +12,10 @@ class BaseActionRule(models.Model):
         'generic.condition', 'base_action_rule_post_condition_rel',
         string='Post Conditions',  help="Post conditions (Generic conditions)")
 
-    def onchange_kind(self, cr, uid, ids, kind, context=None):
+    # TODO: rewrite in new API
+    def onchange_kind(self, cr, uid, ids,
+                      kind, context=None
+                      ):  # pylint: disable=old-api7-method-defined
         res = super(BaseActionRule, self).onchange_kind(
             cr, uid, ids, kind, context=context)
 
@@ -21,7 +24,10 @@ class BaseActionRule(models.Model):
 
         return res
 
-    def onchange_model_id(self, cr, uid, ids, model_id, context=None):
+    # TODO: rewrite in new API
+    def onchange_model_id(self, cr, uid, ids,
+                          model_id, context=None
+                          ):  # pylint: disable=old-api7-method-defined
         res = super(BaseActionRule, self).onchange_model_id(
             cr, uid, ids, model_id, context=context)
 
