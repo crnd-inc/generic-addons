9a72c42c01df00c04e7ea9f82cb387f31aee6ede
Дмитро Катюха <dmytro.katyukha@maao.com.ua>
2017-07-25 17:24:10 +0300

    Added addon `generic_m2o`. it replaces widget polymorphic

---
+++ generic_m2o/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Generic M2O Widget",
+    "version": "9.0.0.0.1",
+    "author": "Management and Accounting Online",
+    "website": "https://maao.com.ua",
+    "license": "Other proprietary",
+    "summary": "Generic Many2one widget",
+    'category': 'Web',
+    'depends': [
+        'web',
+    ],
+    'data': [
+        'web_assets.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
