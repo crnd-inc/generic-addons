7a091aa67efd66f37d2043cb6478ce878e6525df
Дмитро Катюха <dmytro.katyukha@maao.com.ua>
2017-07-26 19:53:16 +0300

    new `generic_mixin` addon, request.category:parent_id
    Added `parent_id` field to request.category model,
    Also added basic implementation of `generic_mixin` addon, which at this
    moment contains only one mixin: generic.mixin.parent.names. This mixin
    implements recursion check and basic name_get / name_search for models
    with parent / child relationship

---
+++ generic_mixin/README.rst
@@ -0,0 +1,5 @@
+Generic Mixins
+==============
+
+This is technical addon, that contains model mixins, that may be useful
+in develompent of other addons.
---
+++ generic_mixin/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Generic Mixin",
+
+    'summary': """
+    Technical module with generic mixins, that may help to build other modules
+    """,
+
+    'author': "Management and Accounting Online",
+    'website': "https://maao.com.ua",
+
+    'category': 'Technical Settings',
+    'version': '9.0.0.0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': [
+        'base',
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'Other proprietary',
+}
