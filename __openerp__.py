d4449861f24df120b7933bc5c4509793c8f27e7f
Дмитро Катюха <dmytro.katyukha@maao.com.ua>
2017-10-04 16:00:23 +0300

    Initial port to 11.0
    Following addons disbaled:
    - generic_condition_action_rule - depends on `base_action_rule` that was
      renamed
    - generic_condition_marketing - marketing campaigns moved to enterprise
    - generic_tag_automation - depends on `base_action_rule` that was
      renamed
    - ggeneric_tag_test_automation - depends on `base_action_rule` that was
      renamed

--- __openerp__.py
+++ __openerp__.py
@@ -9,7 +9,7 @@
     'website': "https://maao.com.ua",
 
     'category': 'Generic Resource',
-    'version': '10.0.0.0.2',
+    'version': '11.0.0.0.2',
 
     # any module necessary for this one to work correctly
     'depends': [
