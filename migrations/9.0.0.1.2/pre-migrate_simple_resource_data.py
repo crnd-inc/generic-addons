a3dce56478433049f33fb10f763a248982e515c0
Дмитро Катюха <dmytro.katyukha@maao.com.ua>
2018-01-29 18:31:06 +0200

    fix generic_resource_simple migration

--- migrations/9.0.0.1.2/pre-migrate_simple_resource_data.py
+++ migrations/9.0.0.1.2/pre-migrate_simple_resource_data.py
@@ -7,11 +7,13 @@ def migrate(cr, installed_version):
         SET module='generic_resource'
         WHERE module = 'generic_resource_simple'
           AND model in ('generic.resource',
+                        'generic.resource.type',
                         'generic.resource.simple',
-                        'generic.resource.simple.category');
-        UPDATE ir_model_data
-        SET module='generic_resource'
-        WHERE module = 'generic_resource_simple'
-          AND name IN ('generic_resource_type_default',
-                       'generic_resource_simple_sequence');
+                        'generic.resource.simple.category',
+                        'ir.model',
+                        'ir.model.fields',
+                        'ir.ui.menu',
+                        'ir.ui.view',
+                        'ir.sequence',
+                        'ir.actions.act_window');
     """)
