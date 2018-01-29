024325c2584b291a40bc54888ed37b337aceec38
Дмитро Катюха <dmytro.katyukha@maao.com.ua>
2018-01-29 10:56:22 +0200

    Added chatter to services, minor fixes

--- models/generic_service.py
+++ models/generic_service.py
@@ -5,8 +5,11 @@ from openerp import models, fields
 
 class GenericSerivce(models.Model):
     _name = 'generic.service'
+    _inherit = 'mail.thread'
     _description = 'Generic Service'
 
-    name = fields.Char(translate=True, required=True, index=True)
-    active = fields.Boolean(default=True, index=True)
+    name = fields.Char(
+        translate=True, required=True, index=True, track_visibility='always')
+    active = fields.Boolean(
+        default=True, index=True, track_visibility='onchange')
     description = fields.Text(translate=True)
