1f9e8913e986d41336bd2e7f01ce4409f6a653d3
Ірина Росколуп <roskolup@hp-z210-3.crnd.int>
2018-08-31 12:47:03 +0300

    Generic service view placeholder for service data

--- __openerp__.py
+++ __openerp__.py
@@ -8,7 +8,7 @@
     'website': "https://crnd.pro",
 
     'category': 'Generic Service',
-    'version': '11.0.0.0.2',
+    'version': '11.0.1.0.0',
 
     # any module necessary for this one to work correctly
     'depends': [
