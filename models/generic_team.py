2cd38d20a99c9819d091b3c2bd6cc69693e4f6d1
Ірина Росколуп <roskolup@hp-z210-3.crnd.int>
2018-09-04 17:11:44 +0300

    fixed bugs 1

--- models/generic_team.py
+++ models/generic_team.py
@@ -5,12 +5,15 @@ class GenericTeam(models.Model):
     _name = 'generic.team'
     _inherit = 'mail.thread'
 
-    name = fields.Char(index=True, required=True, translate=True, string='Team name')
-    description = fields.Text(translate=True, string='Description')
+    name = fields.Char(index=True, required=True, translate=True,
+                       string='Team name')
+    description = fields.Text(translate=True)
     active = fields.Boolean(index=True, default=True, string='Active?')
-    leader_id = fields.Many2one('res.users', required=True, index=True, string='Team leader')
+    leader_id = fields.Many2one('res.users', required=True, index=True,
+                                string='Team leader')
     user_ids = fields.Many2many('res.users', string='Team members')
-    user_count = fields.Integer(compute='_compute_user_count', readonly=True, string='Users count')
+    user_count = fields.Integer(compute='_compute_user_count',
+                                readonly=True, string='Users count')
 
     @api.multi
     def _compute_user_count(self):
