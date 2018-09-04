d2c5464bac5c452a7df380c2f4f7a27d307bc0cb
Ірина Росколуп <roskolup@hp-z210-3.crnd.int>
2018-09-04 16:47:25 +0300

    add module generic_team

---
+++ __manifest__.py
@@ -0,0 +1,26 @@
+{
+    'name': "Generic team",
+
+    'summary': """
+        Generic team.
+    """,
+
+    'author': "Center of Research & Development",
+    'website': "https://crnd.pro",
+
+    'category': 'Generic team',
+    'version': '11.0.1.0.0',
+
+    "depends": [
+        'base',
+        'mail',
+    ],
+
+    "data": [
+        'views/generic_team_menu.xml',
+        'views/generic_team.xml',
+    ],
+    "installable": True,
+    "application": False,
+    'license': 'Other proprietary',
+}
