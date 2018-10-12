408e39a560c3421368cd12badf1f9925a55fe98d
karavaiev <sprt.vk@gmail.com>
2018-10-12 15:27:15 +0300

    change version in generic_tag_test_automation/__openerp__.py

--- generic_tag_test_automation/__openerp__.py
+++ generic_tag_test_automation/__openerp__.py
@@ -7,7 +7,7 @@
     'author': "Center of Research & Development",
     'website': "https://crnd.pro",
     'category': 'Uncategorized',
-    'version': '11.0.1.0.0',
+    'version': '11.0.1.0.1',
 
     "depends": [
         "generic_tag_automation",
