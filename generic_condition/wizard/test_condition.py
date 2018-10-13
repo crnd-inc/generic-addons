f30371bf59ffe38729b7aa5f74e28a45c26e9c49
Dmytro Katyukha <cto@crnd.pro>
2018-10-13 18:27:48 +0300

    generic_condition: Add traceback to condition test wizard's debug log

--- generic_condition/wizard/test_condition.py
+++ generic_condition/wizard/test_condition.py
@@ -1,3 +1,5 @@
+import traceback
+
 from openerp import models, fields, api
 from openerp.tools.translate import _
 from openerp.tools import ustr
@@ -22,49 +24,73 @@ class TestGenericCondition(models.TransientModel):
     res_id = fields.Integer(
         'Object ID', required=True,
         help='ID of object to test condition on')
+    test_as_user_id = fields.Many2one('res.users')
     result = fields.Text(readonly=True)
     debug_log = fields.Html(readonly=True)
 
+    def _get_record_internal(self):
+        TestModel = self.env[self.condition_id.model_id.model]
+        record = TestModel.browse(self.res_id)
+        if not record.exists():
+            raise ValidationError(
+                _('Object (model: %s; id: %s) not found'
+                  '') % (self.condition_id.model_id.model, self.res_id))
+        return record
+
+    def _get_record(self):
+        record = self._get_record_internal()
+        if self.test_as_user_id:
+            record = record.sudo(self.test_as_user_id)
+        return record
+
+    def _adapt_result(self, result):
+        if result is True:
+            return _('Ok')
+        elif result is False:
+            return _('Fail')
+        elif isinstance(result, Exception):
+            return _('Error')
+        return _('Unknown')
+
+    def _compute_result(self, debug_log):
+        cache = dict()
+        record = self._get_record()
+        if self.test_as_user_id:
+            return self.condition_id.sudo(self.test_as_user_id).check(
+                record, cache=cache, debug_log=debug_log)
+
+        return self.condition_id.check(
+            record, cache=cache, debug_log=debug_log)
+
+    def _get_result(self, debug_log):
+        try:
+            with self.env.cr.savepoint():
+                result = self._compute_result(debug_log)
+        except Exception as exc:
+            result = str(exc)
+            self.condition_id._debug_log(
+                debug_log,
+                self._get_record_internal(),
+                "<pre>%s</pre>" % traceback.format_exc())
+        return self._adapt_result(result)
+
     @api.multi
     def process(self):
-        result_map = {
-            True: _('Ok'),
-            False: _('Fail'),
-            None: _('Unknown'),
-        }
-
-        for wiz in self:
-            TestModel = self.env[wiz.condition_id.model_id.model]
-
-            if not TestModel.search([('id', '=', wiz.res_id)]):
-                raise ValidationError(
-                    _('Object (model: %s; id: %s) not found'
-                      '') % (wiz.condition_id.model_id.model, wiz.res_id))
-            else:
-                debug_log = DebugLogger()
-                cache = dict()
-
-                test_obj = TestModel.browse(wiz.res_id)
-                result = wiz.condition_id.check(
-                    test_obj, cache=cache, debug_log=debug_log)
-                result = result_map.get(result, result)
-
-                wiz.write({
-                    'result': ustr(result),
-                    'debug_log': debug_log.get_log_html(),
-                })
-
-            return {
-                'type': 'ir.actions.act_window',
-                'res_model': 'generic.condition.test_condition',
-                'view_mode': 'form',
-                'view_type': 'form',
-                'res_id': wiz.id,
-                'views': [(False, 'form')],
-                'target': 'new',
-                'context': dict(self.env.context),
-            }
+        self.ensure_one()
+        debug_log = DebugLogger()
+        result = self._get_result(debug_log)
+        self.write({
+            'result': ustr(result),
+            'debug_log': debug_log.get_log_html(),
+        })
 
         return {
-            'type': 'ir.actions.act_window_close',
+            'type': 'ir.actions.act_window',
+            'res_model': 'generic.condition.test_condition',
+            'view_mode': 'form',
+            'view_type': 'form',
+            'res_id': self.id,
+            'views': [(False, 'form')],
+            'target': 'new',
+            'context': dict(self.env.context),
         }
