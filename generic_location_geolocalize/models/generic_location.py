904ff9cb7e668bcc720842447df0dc2a7bb0cedc
Dmytro Katyukha <cto@crnd.pro>
2018-10-26 12:00:51 +0300

    geolocalize fix: changes in google api and odoo api
    Odoo commit: https://github.com/odoo/odoo/commit/e6ca846c65bd94bf4c3ecb57847e93ae8ecaa9e9

--- generic_location_geolocalize/models/generic_location.py
+++ generic_location_geolocalize/models/generic_location.py
@@ -8,24 +8,30 @@ from odoo.addons.base_geolocalize.models.res_partner import (
 class GenericLocation(models.Model):
     _inherit = 'generic.location'
 
+    @classmethod
+    def _geo_localize(cls, apikey, street='', zip_code='', city='',
+                      state='', country=''):
+        search = geo_query_address(
+            street=street, zip=zip_code, city=city,
+            state=state, country=country)
+        result = geo_find(search, apikey)
+        if result is None:
+            search = geo_query_address(city=city, state=state, country=country)
+            result = geo_find(search, apikey)
+        return result
+
     @api.multi
     def geo_localize(self):
+        apikey = self.env['ir.config_parameter'].sudo().get_param(
+            'google.api_key_geocode')
         for record in self.with_context(lang='en_US'):
-            result = geo_find(geo_query_address(
+            result = record._geo_localize(
+                apikey,
                 street=record.street,
-                zip=record.zip,
+                zip_code=record.zip,
                 city=record.city,
                 state=record.state_id.name,
-                country=record.country_id.name
-            ))
-
-            if result is None:
-                result = geo_find(geo_query_address(
-                    city=record.city,
-                    state=record.state_id.name,
-                    country=record.country_id.name
-                ))
-
+                country=record.country_id.name)
             if result:
                 record.write({
                     'latitude': result[0],
