4b62b7048a998632157cc494989431e3858c4147
Вадим Караваєв <karavaiev@bezobiuk.crnd.int>
2018-10-29 15:39:51 +0200

    modified manifests (changed versions)

--- __openerp__.py
+++ __openerp__.py
@@ -9,9 +9,8 @@
 
     'author': "Center of Research & Development",
     'website': "https://crnd.pro",
-
     'category': 'Generic Service',
-    'version': '11.0.1.2.0',
+    'version': '11.0.1.2.1',
 
     # any module necessary for this one to work correctly
     'depends': [
