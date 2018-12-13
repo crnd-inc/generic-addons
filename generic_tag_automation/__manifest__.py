b0900293f5d19de1be3a46b63d59fbc08da35f7c
Вадим Караваєв <karavaiev@bezobiuk.crnd.int>
2018-12-13 13:27:43 +0200

    translated modules

--- generic_tag_automation/__manifest__.py
+++ generic_tag_automation/__manifest__.py
@@ -7,7 +7,7 @@
     'author': "Center of Research & Development",
     'website': "https://crnd.pro",
     'category': 'Generic Tags',
-    'version': '11.0.1.0.6',
+    'version': '11.0.1.0.7',
 
     "depends": [
         "generic_tag",
