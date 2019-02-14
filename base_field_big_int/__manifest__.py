0491f1b151bc70b7da34fad66f997e7dfb4d09e2
Вадим Караваєв <karavaiev@bezobiuk.crnd.int>
2019-02-14 16:32:53 +0200

    changed versions, update icon for base_field_big_int

--- base_field_big_int/__manifest__.py
+++ base_field_big_int/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Base Field Big Int",
-    "version": "11.0.0.0.3",
+    "version": "12.0.0.0.3",
     "author": "Center of Research and Development",
     "website": "https://crnd.pro",
     "license": "LGPL-3",
