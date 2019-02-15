08165b4eebe59223aaa0b50aa1265e557ddcbaf2
Ірина Росколуп <roskolup@hp-z210-3.crnd.int>
2019-02-15 12:28:07 +0200

    version

--- generic_condition/__manifest__.py
+++ generic_condition/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Generic Condition",
-    "version": "11.0.1.2.17",
+    "version": "11.0.1.2.18",
     "author": "Center of Research and Development",
     "website": "https://crnd.pro",
     "license": "LGPL-3",
