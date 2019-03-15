039cd16865a4be6b3bc23db48308c84479537ada
Ірина Росколуп <roskolup@hp-z210-3.crnd.int>
2019-03-15 12:05:58 +0200

    final

--- migrations/11.0.1.1.1/pre-migrate.py
+++ migrations/11.0.1.1.1/pre-migrate.py
@@ -10,44 +10,46 @@ def migrate(cr, installed_version):
     """)
 
     cr.execute("""
-        select id from ir_model
-        where name='generic.team.member';
+        SELECT id FROM ir_model
+        WHERE name='generic.team.member';
     """)
 
     if cr.rowcount == 0:
         cr.execute("""
-            insert into ir_model (name, model)
-            values ('generic.team.member', 'generic.team.member')
-            returning id;
+            INSERT INTO ir_model (name, model)
+            VALUES ('generic.team.member', 'generic.team.member')
+            RETURNING ID;
         """)
         model_id = cr.fetchone()[0]
     else:
         model_id = cr.fetchone()[0]
 
     cr.execute("""
-        select *
-        from ir_model_access
-        where name like 'generic_team_member';
+        SELECT *
+        FROM ir_model_access
+        WHERE name='generic_team_member';
     """)
     cn = cr.rowcount
 
     if cn == 0:
         cr.execute("""
-            insert into ir_model_access (name, model_id)
-            values
+            INSERT INTO ir_model_access (name, model_id)
+            VALUES
             ('generic_team_member_user_implicit', %(model_id)s),
             ('generic_team_member_user', %(model_id)s),
             ('generic_team_member_manager', %(model_id)s);
         """, {'model_id': model_id})
 
     cr.execute("""
-        select * from ir_model_data
-        where name='model_generic_team_member' and module='generic_team';
+        SELECT * FROM ir_model_data
+        WHERE name='model_generic_team_member' AND module='generic_team';
     """)
     cn = cr.rowcount
 
     if cn == 0:
         cr.execute("""
-            insert into ir_model_data (name, module, model, res_id)
-            values ('model_generic_team_member', 'generic_team', 'ir.model', %(model_id)s);
+            INSERT INTO ir_model_data (name, module, model, res_id)
+            VALUES (
+            'model_generic_team_member',
+            'generic_team', 'ir.model', %(model_id)s);
         """, {'model_id': model_id})
