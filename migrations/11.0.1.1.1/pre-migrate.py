e0f1e90763584a12894240f223077825b633204b
Dmytro Katyukha <cto@crnd.pro>
2019-03-19 14:23:00 +0200

    generic_team: Fix Migration

--- migrations/11.0.1.1.1/pre-migrate.py
+++ migrations/11.0.1.1.1/pre-migrate.py
@@ -1,40 +1,30 @@
+from odoo.tools.sql import (
+    create_model_table,
+    create_column,
+    add_foreign_key,
+)
+
+
 def migrate(cr, installed_version):
+    create_model_table(cr, 'generic_team_member')
+    create_column(cr, 'generic_team_member', 'team_id', 'integer')
+    create_column(cr, 'generic_team_member', 'user_id', 'integer')
+    add_foreign_key(
+        cr, 'generic_team_member', 'team_id', 'generic_team', 'id', 'cascade')
+    add_foreign_key(
+        cr, 'generic_team_member', 'user_id', 'res_users', 'id', 'cascade')
+
     cr.execute("""
-        ALTER TABLE IF EXISTS generic_team_res_users_rel
-            RENAME TO generic_team_member;
-        ALTER TABLE IF EXISTS generic_team_member
-            RENAME COLUMN generic_team_id
-               TO team_id;
-        ALTER TABLE IF EXISTS generic_team_member
-            RENAME COLUMN res_users_id
-               TO user_id;
+        INSERT INTO generic_team_member (user_id, team_id)
+        (SELECT res_users_id, generic_team_id
+        FROM generic_team_res_users_rel);
+
+        DROP TABLE generic_team_res_users_rel;
+        DELETE FROM ir_model_relation
+        WHERE name = 'generic_team_res_users_rel';
+
+        DELETE FROM ir_model_constraint AS imc
+        WHERE model = (
+               SELECT id FROM ir_model AS im WHERE im.model = 'generic.team')
+          AND imc.name ILIKE 'generic_team_res_users_rel_%';
     """)
-
-    # cr.execute("""
-        # SELECT id FROM ir_model
-        # WHERE name='generic.team.member';
-    # """)
-
-    # if cr.rowcount == 0:
-        # cr.execute("""
-            # INSERT INTO ir_model (name, model)
-            # VALUES ('generic.team.member', 'generic.team.member')
-            # RETURNING ID;
-        # """)
-        # model_id = cr.fetchone()[0]
-    # else:
-        # model_id = cr.fetchone()[0]
-
-    # cr.execute("""
-        # SELECT * FROM ir_model_data
-        # WHERE name='model_generic_team_member' AND module='generic_team';
-    # """)
-    # cn = cr.rowcount
-
-    # if cn == 0:
-        # cr.execute("""
-            # INSERT INTO ir_model_data (name, module, model, res_id)
-            # VALUES (
-            # 'model_generic_team_member',
-            # 'generic_team', 'ir.model', %(model_id)s);
-        # """, {'model_id': model_id})
