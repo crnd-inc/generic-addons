0e1735093c8b885826e8c5f101db6f7c53afeec8
Dmytro Katyukha <cto@crnd.pro>
2019-05-10 16:23:09 +0300

    Refactor tests for generic resource roles

--- tests/test_generic_resource_mixin_inv_number.py
+++ tests/test_generic_resource_mixin_inv_number.py
@@ -6,22 +6,26 @@ class TestGenericResourceMixinInvNumber(TransactionCase):
     def test_create_method(self):
 
         SimpleResource = self.env['generic.resource.simple']
-        category = self.env.ref('generic_resource.'
-                                'simple_resource_category_'
-                                'equipment_computers_desktops')
+        category = self.env.ref(
+            'generic_resource.'
+            'simple_resource_category_equipment_computers_desktops')
 
-        resource = SimpleResource.create({'name': 'test_name',
-                                          'category_id': category.id})
+        resource = SimpleResource.create({
+            'name': 'test_name',
+            'category_id': category.id,
+        })
 
-        # test the inv_number when copying a record
+        # test that inv_number changed when copying a record
         self.assertNotEqual(resource.inv_number, resource.copy().inv_number)
         # ---
 
         self.assertEqual(resource.inv_number[:2], 'SR')
 
-        resource = SimpleResource.create({'name': 'test_name',
-                                          'category_id': category.id,
-                                          'inv_number': 'TEST-INV-NUMBER'})
+        resource = SimpleResource.create({
+            'name': 'test_name',
+            'category_id': category.id,
+            'inv_number': 'TEST-INV-NUMBER',
+        })
 
         self.assertEqual(resource.inv_number, 'TEST-INV-NUMBER')
         self.assertEqual(resource.display_name, 'test_name [TEST-INV-NUMBER]')
