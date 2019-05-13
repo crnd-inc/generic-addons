56825fc90e204c00b19253a63703344e42f0d902
roskolup <irina.roskolup@gmail.com>
2019-05-13 16:55:38 +0300

    version

--- __manifest__.py
+++ __manifest__.py
@@ -10,7 +10,7 @@
     'author': "Center of Research and Development",
     'website': "https://crnd.pro",
     'category': 'Generic Service',
-    'version': '11.0.1.3.1',
+    'version': '11.0.1.3.2',
 
     # any module necessary for this one to work correctly
     'depends': [
