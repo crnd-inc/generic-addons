25c2d7bdebff2f1558c90e170bd846e19ede95c8
Вадим Караваєв <karavaiev@hp-z210-user3.crnd.int>
2019-08-29 13:09:31 +0300

    added _description to generic_mixin/models/generic_track_changes.py

--- generic_mixin/__manifest__.py
+++ generic_mixin/__manifest__.py
@@ -9,7 +9,7 @@
     'website': "https://crnd.pro",
 
     'category': 'Technical Settings',
-    'version': '11.0.1.1.1',
+    'version': '11.0.1.1.2',
 
     # any module necessary for this one to work correctly
     'depends': [
