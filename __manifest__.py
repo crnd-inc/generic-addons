6abc7decfcb2a5840fc66b5d1ea32eeca26f6f21
Вадим Караваєв <karavaiev@hp-z210-user3.crnd.int>
2019-09-12 17:54:28 +0300

    updated versions

--- __manifest__.py
+++ __manifest__.py
@@ -8,7 +8,7 @@
     'author': "Center of Research and Development",
     'website': "https://crnd.pro",
     'category': 'Generic Service',
-    'version': '11.0.1.3.11',
+    'version': '11.0.1.4.0',
 
     # any module necessary for this one to work correctly
     'depends': [
