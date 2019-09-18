72592b1b1fe45911270e71616061e22a1865346c
Вадим Караваєв <karavaiev@hp-z210-user3.crnd.int>
2019-09-18 13:53:43 +0300

    minor fixes

--- __manifest__.py
+++ __manifest__.py
@@ -10,11 +10,7 @@
     'website': "https://crnd.pro",
 
     'category': 'Generic Resource',
-<<<<<<< HEAD
-    'version': '12.0.1.7.0',
-=======
-    'version': '11.0.1.8.0',
->>>>>>> updated versions
+    'version': '12.0.1.8.0',
 
     # any module necessary for this one to work correctly
     'depends': [
