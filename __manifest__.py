bba4233d2591bc01811a14a322bf6875e70fa599
Dmytro Katyukha <cto@crnd.pro>
2019-10-18 18:54:07 +0300

    Minor fixes, smae name for two fields, etc

--- __manifest__.py
+++ __manifest__.py
@@ -10,7 +10,7 @@
     'website': "https://crnd.pro",
 
     'category': 'Generic Resource',
-    'version': '11.0.1.12.1',
+    'version': '11.0.1.12.2',
 
     # any module necessary for this one to work correctly
     'depends': [
