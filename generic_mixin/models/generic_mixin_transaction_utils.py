ce2e17a4cacab69b7c6c47f4d95914fed335e776
Dmytro Katyukha <cto@crnd.pro>
2019-12-05 14:16:26 +0200

    [LINT] Fix lint issues for generic_mixin

--- generic_mixin/models/generic_mixin_transaction_utils.py
+++ generic_mixin/models/generic_mixin_transaction_utils.py
@@ -8,20 +8,33 @@ _logger = logging.getLogger(__name__)
 class GenericMixinTransactionUtils(models.AbstractModel):
     """ Simple mixin that contains utility methods related to
         transaction management in Odoo.
+
+        This mixin is useful for long-running operations and helps to
+        avoid deadlocks.
+
+        For example, you may write code like following in some scheduler:
+
+            for record in records:
+                with record._in_new_transaction() as nrec:
+                    nrec._lock_for_update()
+                    # do your long-runnning operation and be sure, that if
+                    # record was precessed successufully changes will be
+                    # commited.
     """
     _name = 'generic.mixin.transaction.utils'
     _description = 'GenericMixin: Transaction Utils'
 
     def _lock_for_update(self):
-        """ Lock selected records for update
+        """ Lock selected records for update.
         """
         if self:
+            # pylint: disable=sql-injection
             self.env.cr.execute("""
                 SELECT id
                 FROM "{table_name}"
                 WHERE id IN %(ids)s
                 FOR UPDATE NOWAIT;
-            """.format(table_name=self._table), {
+            """.format(table_name=self._table), {  # nosec
                 'ids': tuple(self.ids),
             })
 
