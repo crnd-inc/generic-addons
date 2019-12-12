96cc5dc9cafc425a531a01d7654fc9b8f3125edc
Dmytro Katyukha <cto@crnd.pro>
2019-12-12 18:58:41 +0200

    generic_mixin: _in_new_transaction now has no_raise and lock args

--- generic_mixin/__manifest__.py
+++ generic_mixin/__manifest__.py
@@ -9,7 +9,7 @@
     'website': "https://crnd.pro",
 
     'category': 'Technical Settings',
-    'version': '11.0.1.4.0',
+    'version': '11.0.1.5.0',
 
     # any module necessary for this one to work correctly
     'depends': [
