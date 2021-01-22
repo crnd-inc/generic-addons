18d4524ec72601c414e04e19864ae01cf3ef9eb9
Dmytro Katyukha <cto@crnd.pro>
2021-01-22 13:16:22 +0200

    Added context-switch to be able to use default name_get
    (for parent.names mixin and for generic.tag model)

--- generic_mixin/__manifest__.py
+++ generic_mixin/__manifest__.py
@@ -9,7 +9,7 @@
     'website': "https://crnd.pro",
 
     'category': 'Technical Settings',
-    'version': '12.0.1.18.0',
+    'version': '12.0.1.19.0',
 
     # any module necessary for this one to work correctly
     'depends': [
