07fefa09f858c31f8eb75013b63bbe634e5d6080
Dmytro Katyukha <cto@crnd.pro>
2021-10-15 13:04:13 +0300

    Move generic_resource_mixin_inv_number to separate file

--- models/__init__.py
+++ models/__init__.py
@@ -3,6 +3,7 @@ from . import (
     generic_resource_type,
     generic_resource_related_mixin,
     generic_resource_mixin,
+    generic_resource_mixin_inv_number,
     generic_resource_simple,
     generic_resource_simple_category,
     generic_resource_wizard_mixin,
