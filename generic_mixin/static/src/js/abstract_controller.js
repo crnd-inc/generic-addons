990d1bdb56723e7955bedf5d935a8c58bd681ba9
rymar.roman <rymar.roman@crnd.pro>
2021-11-30 12:46:47 +0200

    generic_mixin_refresh_view porting to v14

---
+++ generic_mixin/static/src/js/abstract_controller.js
@@ -0,0 +1,13 @@
+/* global Promise*/
+odoo.define('generic_mixin.AbstractController', function (require) {
+    "use strict";
+
+    require('web.AbstractController').include({
+
+        init: function (parent, model, renderer, params) {
+            this._super.apply(this, arguments);
+
+            this.isMultiRecord = params.isMultiRecord;
+        },
+    });
+});
---
+++ generic_mixin/static/src/js/abstract_view.js
@@ -0,0 +1,13 @@
+/* global Promise*/
+odoo.define('generic_mixin.AbstractView', function (require) {
+    "use strict";
+
+    require('web.AbstractView').include({
+
+        init: function () {
+            this._super.apply(this, arguments);
+
+            this.controllerParams.isMultiRecord = this.multi_record;
+        },
+    });
+});
