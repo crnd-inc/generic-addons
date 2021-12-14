9f75c2460218a0ccc0f90ad35a412552eb8df0a4
rymar.roman <rymar.roman@crnd.pro>
2021-12-14 11:30:05 +0200

    generic_mixin_refresh_view add visualize change on kanban view

--- generic_mixin/static/src/js/list_renderer.js
+++ generic_mixin/static/src/js/list_renderer.js
@@ -7,6 +7,12 @@ odoo.define('generic_mixin.ListRenderer', function (require) {
 
     require('web.ListRenderer').include({
 
+        init: function () {
+            this._super.apply(this, arguments);
+            this._generic_refresh_mixin__visualization_on_timeout = 100;
+            this._generic_refresh_mixin__visualization_off_timeout = 1200;
+        },
+
         _renderRows: function () {
             var rows = this._super.apply(this, arguments);
             this.generic_refresh_view__clear_refresh_ids();
@@ -15,9 +21,33 @@ odoo.define('generic_mixin.ListRenderer', function (require) {
 
         _renderRow: function (record) {
             var $tr = this._super.apply(this, arguments);
-            this.generic_mixin_refresh_view__visualize_list_row_changes(
+            this._generic_mixin_refresh_view__visualize_changes(
                 $tr, record.res_id);
             return $tr;
+        },
+
+        _generic_mixin_refresh_view__visualize_changes: function (
+            $tr, res_id) {
+            if (this._generic_refresh_mixin__refresh_ids.create &&
+                this._generic_refresh_mixin__refresh_ids.write) {
+                if (this._generic_refresh_mixin__refresh_ids.create.includes(
+                    res_id)) {
+                    setTimeout(function () {
+                        $tr.addClass('gmrv_highlighting_record_create');
+                    }, this._generic_refresh_mixin__visualization_on_timeout);
+                    setTimeout(function () {
+                        $tr.removeClass('gmrv_highlighting_record_create');
+                    }, this._generic_refresh_mixin__visualization_off_timeout);
+                } else if (this._generic_refresh_mixin__refresh_ids.write
+                    .includes(res_id)) {
+                    setTimeout(function () {
+                        $tr.addClass('gmrv_highlighting_record_write');
+                    }, this._generic_refresh_mixin__visualization_on_timeout);
+                    setTimeout(function () {
+                        $tr.removeClass('gmrv_highlighting_record_write');
+                    }, this._generic_refresh_mixin__visualization_off_timeout);
+                }
+            }
         },
     });
 });
--- generic_mixin/static/src/js/refresh_view_mixin.js
+++ generic_mixin/static/src/js/refresh_view_mixin.js
@@ -7,8 +7,6 @@ odoo.define('generic_mixin.RefreshViewMixin', function () {
             this._super.apply(this, arguments);
             this.generic_refresh_view__is_compatible = true;
             this._generic_refresh_mixin__refresh_ids = {};
-            this._generic_refresh_mixin__visualization_on_timeout = 100;
-            this._generic_refresh_mixin__visualization_off_timeout = 1200;
         },
 
         generic_refresh_view__set_refresh_ids: function (refresh_ids) {
@@ -19,30 +17,6 @@ odoo.define('generic_mixin.RefreshViewMixin', function () {
 
         generic_refresh_view__clear_refresh_ids: function () {
             this._generic_refresh_mixin__refresh_ids = {};
-        },
-
-        generic_mixin_refresh_view__visualize_list_row_changes: function (
-            $tr, res_id) {
-            if (this._generic_refresh_mixin__refresh_ids.create &&
-                this._generic_refresh_mixin__refresh_ids.write) {
-                if (this._generic_refresh_mixin__refresh_ids.create.includes(
-                    res_id)) {
-                    setTimeout(function () {
-                        $tr.addClass('gmrv_highlighting_record_create');
-                    }, this._generic_refresh_mixin__visualization_on_timeout);
-                    setTimeout(function () {
-                        $tr.removeClass('gmrv_highlighting_record_create');
-                    }, this._generic_refresh_mixin__visualization_off_timeout);
-                } else if (this._generic_refresh_mixin__refresh_ids.write
-                    .includes(res_id)) {
-                    setTimeout(function () {
-                        $tr.addClass('gmrv_highlighting_record_write');
-                    }, this._generic_refresh_mixin__visualization_on_timeout);
-                    setTimeout(function () {
-                        $tr.removeClass('gmrv_highlighting_record_write');
-                    }, this._generic_refresh_mixin__visualization_off_timeout);
-                }
-            }
         },
     };
 
