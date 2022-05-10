029452cc17f513ef880f5726be4451d233867f96
Dmytro Katyukha <dmytro.katyukha@gmail.com>
2022-05-10 11:20:09 +0300

    location_uuid: Use SQL to ensure uniqueness of uuid in pre-init hook

--- generic_location_uuid/hooks.py
+++ generic_location_uuid/hooks.py
@@ -1,47 +1,50 @@
 import uuid
+import logging
 
-from psycopg2 import sql
 from odoo.tools.sql import (
     create_column,
     column_exists,
 )
 
-
-def _generate_new_uuid(set_uuids):
-    """ Generate new unique UUID, which is not yet in set_uuids
-    """
-    _uuid = str(uuid.uuid4())
-    while _uuid in set_uuids:
-        _uuid = str(uuid.uuid4())
-    return _uuid
+_logger = logging.getLogger(__name__)
 
 
 def pre_init_hook(cr):
-    table = 'generic_location'
-    if column_exists(cr, table, 'uuid'):
+    if column_exists(cr, 'generic_location', 'uuid'):
+        _logger.info(
+            "generic_location: uuid column already exists, "
+            "no need to run pre-init hook")
         return
-    create_column(cr, table, 'uuid', 'VARCHAR')
 
-    # pylint: disable=sql-injection
-    cr.execute(
-        sql.SQL("SELECT array_agg(id) FROM {}").format(sql.Identifier(table)))
+    _logger.info("Running pre_init_hook for geneirc_location_uuid...")
+    create_column(cr, 'generic_location', 'uuid', 'VARCHAR')
+
+    cr.execute("SELECT array_agg(id) FROM generic_location")
     record_ids = cr.fetchone()[0]
     if not record_ids:
         return
 
-    set_uuids = set()
     for record_id in record_ids:
-        _uuid = _generate_new_uuid(set_uuids)
-        set_uuids.add(_uuid)
+        new_uuid_ok = False
+        while not new_uuid_ok:
+            new_uuid = str(uuid.uuid4())
 
-        cr.execute(
-            sql.SQL("""
-                UPDATE {}
-                SET uuid=%(uuid)s
-                WHERE id=%(record_id)s
-            """).format(sql.Identifier(table)),
-            {
-                'uuid': _uuid,
-                'record_id': record_id,
-            }
-        )
+            # Ensure that uuid is unique
+            cr.execute("""
+                SELECT EXISTS (
+                    SELECT 1
+                    FROM generic_location
+                    WHERE uuid = %(uuid)s
+                )
+            """, {'uuid': new_uuid})
+            if not cr.fetchone()[0]:
+                new_uuid_ok = True
+
+        cr.execute("""
+            UPDATE generic_location
+            SET uuid=%(uuid)s
+            WHERE id=%(record_id)s
+        """, {
+            'uuid': new_uuid,
+            'record_id': record_id,
+            })
