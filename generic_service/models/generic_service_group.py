f05ea98266b9ed27616140ab8410e82009a1ff17
o.zabolotska <o.zabolotska@omo.systems>
2022-06-07 16:53:04 +0300

    fix

--- generic_service/models/generic_service_group.py
+++ generic_service/models/generic_service_group.py
@@ -1,4 +1,6 @@
-from odoo import models
+from odoo import models, fields, api
+from odoo.addons.generic_mixin.tools.x2m_agg_utils import read_counts_for_o2m
+
 
 
 class GenericServiceGroup(models.Model):
@@ -8,3 +10,21 @@ class GenericServiceGroup(models.Model):
         'generic.mixin.name_with_code',
         'generic.mixin.uniq_name_code',
     ]
+
+    active = fields.Boolean(index=True, default=True)
+    service_ids = fields.One2many('generic.service', 'name')
+    service_count = fields.Integer(compute='_compute_service_count')
+    show_service_action_id = fields.Many2one(
+        'ir.actions.act_window', readonly=True)
+
+    @api.depends('service_ids')
+    def _compute_service_count(self):
+        mapped_data = read_counts_for_o2m(
+            records=self, field_name='service_ids', sudo=True)
+        for rec in self:
+            rec.service_count = mapped_data.get(rec.id, 0)
+
+    def action_show_service(self):
+        self.ensure_one()
+        if self.sudo().show_service_action_id:
+            return self.sudo().show_service_action_id.read()[0]
