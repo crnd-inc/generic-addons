84c7873a0c6d9f279bd3cd2e401455726aed6b3c
AShurikas <ANechipor@gmail.com>
2022-06-27 18:15:32 +0300

    add tests for leaf

--- generic_condition_test/tests/test_check_find.py
+++ generic_condition_test/tests/test_check_find.py
@@ -6,20 +6,38 @@ class TestConditionCheckFind(SavepointCase):
     @classmethod
     def setUpClass(cls):
         super(TestConditionCheckFind, cls).setUpClass()
-        cls.test_partner = cls.env.ref(
+        cls.test_partner1 = cls.env.ref(
             'generic_condition_test.demo_partner_it_group')
+        cls.test_partner2 = cls.env.ref(
+            'generic_condition_test.demo_partner_consulting_group'
+        )
         cls.condition = cls.env.ref(
             "generic_condition_test"
             ".test_condition_event_year_ago_on_partner_area"
         )
-        cls.condition_leaf = cls.env.ref(
+        cls.condition_location_leaf = cls.env.ref(
             "generic_condition_test"
             ".test_condition_leaf_event_location_equals_partner_location"
         )
+        cls.condition_logic_operator_leaf = cls.env.ref(
+            "generic_condition_test.test_condition_logic_operator"
+        )
 
     def test_condition_type_find_check(self):
-        self.assertTrue(self.condition.check(self.test_partner))
-        self.condition_leaf.write({
-            'value_field_operator': '!=',
+        # Check following conditions:
+        # - partner city same as event location AND
+        # - partner in Attendees;
+        # - event created more than year ago
+        self.assertTrue(self.condition.check(self.test_partner1))
+        self.assertFalse(self.condition.check(self.test_partner2))
+
+        # Change logical operator to 'OR'
+        self.condition_logic_operator_leaf.write({
+            'type': 'operator-or',
         })
-        self.assertFalse(self.condition.check(self.test_partner))
+        # Check following conditions:
+        # - partner city same as event location OR
+        # - partner in Attendees;
+        # - event created more than year ago
+        self.assertTrue(self.condition.check(self.test_partner1))
+        self.assertTrue(self.condition.check(self.test_partner2))
