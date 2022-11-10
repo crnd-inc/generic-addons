afc021a181d764db0fc130f75ac327fe26c56c7a
Dmytro Katyukha <dmytro.katyukha@gmail.com>
2022-11-10 18:31:43 +0200

    Added new meth get_form_action_by_xmlid to generic.mixin.get.action mixin

--- generic_mixin/models/generic_mixin_get_action.py
+++ generic_mixin/models/generic_mixin_get_action.py
@@ -59,3 +59,34 @@ class GenericMixinGetAction(models.AbstractModel):
             action['display_name'] = name
 
         return action
+
+    @api.model
+    def get_form_action_by_xmlid(self, xmlid, context=None,
+                                 res_id=0, name=None):
+        """ Return action based on provided xmlid, that will open form view.
+            Usually, this method could be used in cases, when it is needed
+            to open form view to create new object for user or edit object.
+
+            :param str xmlid: XML (external) ID of action ir.actions.* to read
+            :param dict context: apply new context for action
+            :param int res_id: ID of record to open form for edition.
+                 By default, form will be opened for creation of new record.
+            :param str name: apply new name for action
+                (also changes display name)
+            :return dict: Data for specified action
+        """
+        action = self.get_action_by_xmlid(xmlid, context=context, name=name)
+
+        # Find form view from action:
+        form_view = None
+        for view_id, view_mode in action['views']:
+            if view_mode == 'form':
+                form_view = (view_id, view_mode)
+                break
+        if not form_view:
+            form_view = (False, 'form')
+
+        action['view_mode'] = 'form'
+        action['views'] = [form_view]
+        action['res_id'] = res_id
+        return action
