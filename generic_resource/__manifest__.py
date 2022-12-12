bb8a7e14c924179e8f904fc57ef710de283e4d96
Dmytro Katyukha <dmytro.katyukha@gmail.com>
2022-12-12 11:36:58 +0200

    [FIX] generic_resource: Allow to create models (that are resource)
    by user with administrative access

--- generic_resource/__manifest__.py
+++ generic_resource/__manifest__.py
@@ -10,7 +10,7 @@
     'website': "https://crnd.pro",
 
     'category': 'Generic Resource',
-    'version': '12.0.1.35.0',
+    'version': '12.0.1.36.0',
 
     # any module necessary for this one to work correctly
     'depends': [
