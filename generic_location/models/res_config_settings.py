d02995f76bf35d917c60926a067378cf39ab92ee
Dmytro Katyukha <dmytro.katyukha@gmail.com>
2022-12-19 19:51:08 +0200

    generic_location: Fix test issues

--- generic_location/models/res_config_settings.py
+++ generic_location/models/res_config_settings.py
@@ -5,6 +5,6 @@ class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
     group_use_hierarchical_locations = fields.Boolean(
-        implied_group='generic_location.group_use_hierarchical_location',
+        implied_group='generic_location.group_use_hierarchical_locations',
         string='Use hierarchical locations'
     )
