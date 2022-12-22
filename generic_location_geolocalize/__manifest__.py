0373fa2913188f48a11668896b8715a71f8e676c
Dmytro Katyukha <dmytro.katyukha@gmail.com>
2022-12-22 12:46:51 +0200

    Bump version (to have same minor version for all supported series)

--- generic_location_geolocalize/__manifest__.py
+++ generic_location_geolocalize/__manifest__.py
@@ -9,7 +9,7 @@
     'website': "https://crnd.pro",
 
     'category': 'Generic Location',
-    'version': '12.0.1.3.0',
+    'version': '12.0.1.5.0',
 
     # any module necessary for this one to work correctly
     'depends': [
