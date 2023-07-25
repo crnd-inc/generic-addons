4e3a9149da1f4b90e3a9f8d4a557f0e946617b26
Dmytro Katyukha <dmytro.katyukha@gmail.com>
2023-07-25 13:46:39 +0300

    [REM] Remove obsolete location.address.mixin

--- generic_location/models/generic_location_address_mixin.py
+++
@@ -1,9 +0,0 @@
-from odoo import models
-
-
-# Obsolete, kept for backward compatibility
-# TODO: Remove in 16.0
-class GenericLocationAddressMixin(models.AbstractModel):
-    _name = 'generic.location.address.mixin'
-    _inherit = 'generic.location.mixin'
-    _description = 'Generic Location Address Mixin'
