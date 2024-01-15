608c0ac1d68e5159b0885c3c6719dd692ba4782e
Oleksandr Nechyporenko <a.nechipor@crnd.int>
2024-01-15 10:21:59 +0200

    Fix version

--- generic_mixin/__manifest__.py
+++ generic_mixin/__manifest__.py
@@ -9,7 +9,7 @@
     'website': "https://crnd.pro",
 
     'category': 'Technical Settings',
-    'version': '16.0.1.82.0',
+    'version': '16.0.1.81.2',
 
     # any module necessary for this one to work correctly
     'depends': [
