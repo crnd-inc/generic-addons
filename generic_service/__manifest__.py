d62183f42ebcc8e8b707b2044ce1aa95ce4311e9
Oleksandr Nechyporenko <ANechipor@gmail.com>
2024-03-27 16:54:06 +0200

    Fix version

--- generic_service/__manifest__.py
+++ generic_service/__manifest__.py
@@ -8,7 +8,7 @@
     'author': "Center of Research and Development",
     'website': "https://crnd.pro",
     'category': 'Generic Service',
-    'version': '17.0.1.29.0',
+    'version': '17.0.1.29.1',
 
     # any module necessary for this one to work correctly
     'depends': [
