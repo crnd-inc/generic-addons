a7412e8247e76390670d705922c949ec87add200
Oleksandr Dziuba <dziubaaleksandr22@gmail.com>
2024-04-01 12:41:00 +0300

    [ADD] generic_tag_purchase: Ported to 16

--- generic_tag_purchase/__manifest__.py
+++ generic_tag_purchase/__manifest__.py
@@ -9,7 +9,7 @@
     'website': "https://crnd.pro",
 
     'category': 'Generic Tags',
-    'version': '15.0.1.6.0',
+    'version': '16.0.1.6.0',
 
     "depends": [
         "generic_tag",
@@ -23,7 +23,7 @@
         'views/generic_tag_purchase.xml'
     ],
     'images': ['static/description/banner.png'],
-    "installable": False,
+    "installable": True,
     "auto_install": True,
     'license': 'LGPL-3',
 }
