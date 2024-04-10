58ceaf88cea132237bd40fede714727f333e5947
Oleksandr Dziuba <dziubaaleksandr22@gmail.com>
2024-04-10 20:01:54 +0300

    [FIX] generic_tag_automation: Fixed view

--- generic_tag_automation/__manifest__.py
+++ generic_tag_automation/__manifest__.py
@@ -7,7 +7,7 @@
     'author': "Center of Research and Development",
     'website': "https://crnd.pro",
     'category': 'Generic Tags',
-    'version': '17.0.1.5.0',
+    'version': '17.0.1.5.1',
 
     "depends": [
         "generic_tag",
