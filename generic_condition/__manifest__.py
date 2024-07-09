778d555f1062349108e10b4fb5af7efbfa38c325
vladdemo1 <vladdemo140@gmail.com>
2024-07-09 09:11:39 +0300

    [FIX] fix version

--- generic_condition/__manifest__.py
+++ generic_condition/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Generic Condition",
-    "version": "17.0.1.21.1",
+    "version": "17.0.1.21.2",
     "author": "Center of Research and Development",
     "website": "https://crnd.pro",
     "license": "LGPL-3",
