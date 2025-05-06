f644ebfb48892e4702739ee36eaaceba0464c838
phoenix-git-worker <eternal.worker.email@gmail.com>
2025-05-06 09:38:06 +0300

    [FIX]generic_condition: fix FakeSelection (R10132)

---
+++ generic_condition/changelog/changelog.1.22.3.md
@@ -0,0 +1,2 @@
+- Fix FakeSelection ( extends SelectionField ), delete deprecated code,
+    upgrade existing code according to odoo v18.0.
--- generic_condition/static/src/js/fake_selection.js
+++ generic_condition/static/src/js/fake_selection.js
@@ -7,6 +7,13 @@ import { useService } from "@web/core/utils/hooks";
 const { useState, onPatched } = owl;
 
 class FakeSelection extends SelectionField {
+    static props = {
+        ...SelectionField.props,
+        selectionField: { type: String, optional: true },
+    };
+
+    static supportedTypes = ['char'];
+
     setup() {
         this.orm = useService('orm');
 
@@ -23,10 +30,13 @@ class FakeSelection extends SelectionField {
     get options() {
         return this.state.fakeOptions;
     }
+
     get string() {
-        return this.props.value !== false
-            ? this.options.find((o) => o[0] === this.props.value)[1]
-            : '';
+        if (this.props.value === false) {
+            return '';
+        }
+        const option = this.options.find((o) => o[0] === this.props.value);
+        return option ? option[1] : '';
     }
 
     get selectionFieldId() {
@@ -34,7 +44,7 @@ class FakeSelection extends SelectionField {
             return false;
         }
         const selectionFieldValue = this.props.record.data[this.props.selectionField];
-        if (!selectionFieldValue) {
+        if (!selectionFieldValue || !selectionFieldValue[0]) {
             return false;
         }
         return selectionFieldValue[0];
@@ -47,44 +57,42 @@ class FakeSelection extends SelectionField {
         }
     }
 
-    updateFakeOptions() {
+    async updateFakeOptions() {
         if (!this.selectionFieldId) {
             this.state.fakeOptions = [];
             return;
         }
-        this.orm.call(
-            'ir.model.fields',
-            'get_field_selections',
-            [[this.selectionFieldId]],
-        ).then((data) => {
+        try {
+            const data = await this.orm.call(
+                'ir.model.fields',
+                'get_field_selections',
+                [[this.selectionFieldId]],
+            );
             this.state.fakeOptions = data || [];
-        }).catch((error) => {
+        } catch (error) {
             console.warn('Failed to fetch field selections:', error);
             this.state.fakeOptions = [];
-        });
+        }
     }
 
     onChange(ev) {
         const value = JSON.parse(ev.target.value);
-        this.props.update(value);
+        this.props.record.update(
+            { [this.props.name]: value },
+            { save: this.props.autosave }
+        );
     }
 }
 
-FakeSelection.supportedTypes = ['char'];
-
-FakeSelection.props = {
-    ...SelectionField.props,
-    selectionField: String,
-};
-
-FakeSelection.extractProps = ({ attrs }) => {
+FakeSelection.extractProps = ({ attrs, viewType }, dynamicInfo) => {
     return {
-        ...SelectionField.extractProps,
-        selectionField: attrs.selection_field,
+        ...selectionField.extractProps({ attrs, viewType }, dynamicInfo),
+        selectionField: attrs.selection_field || '',
     };
 };
 
 registry.category('fields').add('fake_selection', {
     ...selectionField,
-    component: FakeSelection
+    component: FakeSelection,
+    extractProps: FakeSelection.extractProps,
 });
