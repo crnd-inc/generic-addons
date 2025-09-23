ef4334fcaa129ea3eceb0978499965ccdab3ccd9
Oleksandr Nechyporenko <ANechipor@gmail.com>
2025-09-23 10:21:06 +0300

    Port locations

--- generic_location/__manifest__.py
+++ generic_location/__manifest__.py
@@ -37,7 +37,7 @@
         'demo/demo_location_tag.xml',
     ],
     'images': ['static/description/banner.png'],
-    'installable': False,
+    'installable': True,
     'application': False,
     "license": "LGPL-3",
 }
