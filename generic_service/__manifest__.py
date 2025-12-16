3e23da80df01edbe1b2aa56b4fa2751168f7c4c4
Oleksandr Nechyporenko <ANechipor@gmail.com>
2025-12-16 17:15:42 +0200

    Fix views

--- generic_service/__manifest__.py
+++ generic_service/__manifest__.py
@@ -8,7 +8,7 @@
     'author': "Center of Research and Development",
     'website': "https://crnd.pro",
     'category': 'Generic Service',
-    'version': "19.0.1.30.2",
+    'version': '19.0.1.30.2',
 
     # any module necessary for this one to work correctly
     'depends': [
