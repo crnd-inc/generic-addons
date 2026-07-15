79d447a0676b520e893a3fb37b49ec6c7625faeb
Dmytro Katyukha <dmytro.katyukha@gmail.com>
2026-07-15 14:28:09 +0300

    Bump versions

--- test_generic_mixin/__manifest__.py
+++ test_generic_mixin/__manifest__.py
@@ -6,7 +6,7 @@
     'author': "Center of Research and Development",
     'website': "https://crnd.pro",
     'category': 'Hidden',
-    'version': '18.0.0.25.4',
+    'version': '18.0.0.25.5',
     'depends': [
         'generic_mixin',
         'calendar',
