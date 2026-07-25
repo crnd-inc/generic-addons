e1f18e3cfb2eb311d7878e723995845644246aae
Dmytro Katyukha <dmytro.katyukha@gmail.com>
2026-07-25 19:28:15 +0300

    [MIG] Migrate `base_field_big_int` to Odoo 19.0

--- base_field_big_int/__manifest__.py
+++ base_field_big_int/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Base Field Big Int",
-    "version": "18.0.0.6.1",
+    "version": "19.0.0.6.1",
     "author": "Center of Research and Development",
     "website": "https://crnd.pro",
     "license": "LGPL-3",
@@ -14,6 +14,6 @@
     'data': [
     ],
     'images': ['static/description/banner.png'],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
 }
--- base_field_big_int/field.py
+++ base_field_big_int/field.py
@@ -2,8 +2,7 @@ from odoo import fields
 
 
 class BigInt(fields.Integer):
-    column_type = ('int8', 'int8')
-    column_cast_from = ('int4',)
+    _column_type = ('int8', 'int8')
 
-    def convert_to_read(self, value, record, use_name_get=True):
+    def convert_to_read(self, value, record, use_display_name=True):
         return value
