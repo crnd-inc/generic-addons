c4974183e08d24940cff78f8635bb84252a3f1ea
Dmytro Katyukha <dmytro.katyukha@gmail.com>
2026-08-12 16:14:19 +0300

    Bump versions

--- generic_mixin/__manifest__.py
+++ generic_mixin/__manifest__.py
@@ -9,7 +9,7 @@
     'website': "https://crnd.pro",
 
     'category': 'Technical Settings',
-    'version': '18.0.1.84.6',
+    'version': '18.0.1.84.7',
 
     # any module necessary for this one to work correctly
     'depends': [
--- test_generic_mixin/__manifest__.py
+++ test_generic_mixin/__manifest__.py
@@ -6,7 +6,7 @@
     'author': "Center of Research and Development",
     'website': "https://crnd.pro",
     'category': 'Hidden',
-    'version': '18.0.0.25.5',
+    'version': '18.0.0.25.6',
     'depends': [
         'generic_mixin',
         'calendar',
